8c61c092de2ff073632eb516b8c5a6bcd48f9a71
maxime.places <maxime.places@ebp.com>
2022-11-28 18:42:41 +0100

    wroker test

--- My.DDD.CQRS.Temp6.WorkService/Program.cs
+++ My.DDD.CQRS.Temp6.WorkService/Program.cs
@@ -6,10 +6,20 @@ namespace My.DDD.CQRS.Temp6.WorkService
   {
     public static void Main(string[] args)
     {
+      // Default Worker implementation
+      //IHost host = Host.CreateDefaultBuilder(args)
+      //    .ConfigureServices(services =>
+      //    {
+      //      services.AddHostedService<Worker>();
+      //    })
+      //    .Build();
+
+      // Timer implementation
       using IHost host = Host.CreateDefaultBuilder(args)
         .ConfigureServices(services =>
         {
           services.AddHostedService<TimerService>();
+          
         })
         .Build();
 
