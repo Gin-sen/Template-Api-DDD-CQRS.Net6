6acf6946821555b627523ca4020ad25d86bdb152
maxime.places <maxime.places@ebp.com>
2022-11-28 18:42:13 +0100

    timer worker test

--- My.DDD.CQRS.Temp6.WorkService/Program.cs
+++ My.DDD.CQRS.Temp6.WorkService/Program.cs
@@ -1,15 +1,17 @@
+using My.DDD.CQRS.Temp6.WorkService.Services;
+
 namespace My.DDD.CQRS.Temp6.WorkService
 {
   public class Program
   {
     public static void Main(string[] args)
     {
-      IHost host = Host.CreateDefaultBuilder(args)
-          .ConfigureServices(services =>
-          {
-            services.AddHostedService<Worker>();
-          })
-          .Build();
+      using IHost host = Host.CreateDefaultBuilder(args)
+        .ConfigureServices(services =>
+        {
+          services.AddHostedService<TimerService>();
+        })
+        .Build();
 
       host.Run();
     }
---
+++ My.DDD.CQRS.Temp6.WorkService/Services/TimerService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My.DDD.CQRS.Temp6.WorkService.Services
+{
+
+  public sealed class TimerService : IHostedService, IAsyncDisposable
+  {
+    private readonly Task _completedTask = Task.CompletedTask;
+    private readonly ILogger<TimerService> _logger;
+    private int _executionCount = 0;
+    private Timer? _timer;
+
+    public TimerService(ILogger<TimerService> logger) => _logger = logger;
+
+    public Task StartAsync(CancellationToken stoppingToken)
+    {
+      _logger.LogInformation("{Service} is running.", nameof(WorkService));
+      _timer = new Timer(DoWork, null, TimeSpan.Zero, TimeSpan.FromSeconds(5));
+
+      return _completedTask;
+    }
+
+    private void DoWork(object? state)
+    {
+      int count = Interlocked.Increment(ref _executionCount);
+
+      _logger.LogInformation(
+          "{Service} is working, execution count: {Count:#,0}",
+          nameof(WorkService),
+          count);
+    }
+
+    public Task StopAsync(CancellationToken stoppingToken)
+    {
+      _logger.LogInformation(
+          "{Service} is stopping.", nameof(WorkService));
+
+      _timer?.Change(Timeout.Infinite, 0);
+
+      return _completedTask;
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+      if (_timer is IAsyncDisposable timer)
+      {
+        await timer.DisposeAsync();
+      }
+
+      _timer = null;
+    }
+  }
+}
