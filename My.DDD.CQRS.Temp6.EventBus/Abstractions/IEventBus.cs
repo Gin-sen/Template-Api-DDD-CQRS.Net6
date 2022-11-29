2f32ec5203610e3fce2b9f094b43163670247ed0
maxime.places <maxime.places@ebp.com>
2022-11-29 02:34:55 +0100

    comment

--- My.DDD.CQRS.Temp6.EventBus/Abstractions/IEventBus.cs
+++ My.DDD.CQRS.Temp6.EventBus/Abstractions/IEventBus.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 namespace My.DDD.CQRS.Temp6.EventBus.Abstractions
 {
   // TODO: https://github.com/dotnet-architecture/eShopOnContainers/tree/main/src/BuildingBlocks/EventBus
+  // https://learn.microsoft.com/en-us/dotnet/architecture/microservices/multi-container-microservice-net-applications/integration-event-based-microservice-communications
+  // https://learn.microsoft.com/en-us/dotnet/architecture/microservices/multi-container-microservice-net-applications/rabbitmq-event-bus-development-test-environment
   public interface IEventBus
     {
     }
