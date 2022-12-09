ba2585a1654d86069d2c89bfeb8897aac5500516
maxime.places <maxime.places@ebp.com>
2022-12-09 04:00:20 +0100

    test

--- My.DDD.CQRS.Temp6.Application/DependencyInjection.cs
+++ My.DDD.CQRS.Temp6.Application/DependencyInjection.cs
@@ -7,6 +7,8 @@ namespace My.DDD.CQRS.Temp6.Application
   {
     public static IServiceCollection AddApplicationLayer(this IServiceCollection services)
     {
+      // a tester
+      // services.AddMediatR(x => x.AsScoped(), typeof(DependencyInjection).Assembly);
       services.AddMediatR(typeof(DependencyInjection).Assembly);
 
       return services;
--- My.DDD.CQRS.Temp6.Contracts/DependencyInjection.cs
+++ My.DDD.CQRS.Temp6.Contracts/DependencyInjection.cs
@@ -8,6 +8,8 @@ namespace My.DDD.CQRS.Temp6.Contracts
     public static IServiceCollection AddContractsLayer(this IServiceCollection services)
     {
 
+      // a tester
+      // services.AddMediatR(x => x.AsScoped(), typeof(DependencyInjection).Assembly);
       services.AddMediatR(typeof(DependencyInjection).Assembly);
 
       return services;
